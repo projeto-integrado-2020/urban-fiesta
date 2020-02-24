46e006423da2098ef1cac2a93ddf3ab0d91134a0
Caio Raposo <caioraposo@protonmail.com>
2020-02-24 21:42:33 +0000

    Add simple rest api and test case

--- src/urban_fiesta/__init__.py
+++ src/urban_fiesta/__init__.py
@@ -3,6 +3,10 @@ import os
 from flask import Flask
 
 from urban_fiesta.settings import config
+from urban_fiesta.api import api
+
+
+__version__ = "0.1.0"
 
 
 def create_app(config_name=None) -> Flask:
@@ -13,4 +17,10 @@ def create_app(config_name=None) -> Flask:
 
     app.config.from_object(config[config_name])
 
+    register_extentions(app)
+
     return app
+
+
+def register_extentions(app: Flask) -> None:
+    api.init_app(app)
