de439c61201664a558bab775006663557e0a4cc6
Caio Raposo <caioraposo@protonmail.com>
2020-02-24 22:18:02 +0000

    Add code coverage report

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,17 @@
 import nox
+from nox.sessions import Session
 
 
 @nox.session(python=["3.8", "3.7"])
-def tests(session):
+def tests(session: Session) -> None:
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
     session.run("pytest", *args)
+
+
+@nox.session(python="3.8")
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    session.run("poetry", "install", external=True)
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
