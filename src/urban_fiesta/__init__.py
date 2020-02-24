c72d113943308d56e508e8dde4042490e8f2f569
Caio Raposo <caioraposo@protonmail.com>
2020-02-24 19:51:37 +0000

    Change to flask's app factory pattern

--- src/urban_fiesta/__init__.py
+++ src/urban_fiesta/__init__.py
@@ -1,1 +1,16 @@
-__version__ = "0.1.0"
+import os
+
+from flask import Flask
+
+from urban_fiesta.settings import config
+
+
+def create_app(config_name=None) -> Flask:
+    if config_name is None:
+        config_name = os.getenv("FLASK_CONFIG", "development")
+
+    app = Flask("urban_fiesta")
+
+    app.config.from_object(config[config_name])
+
+    return app
