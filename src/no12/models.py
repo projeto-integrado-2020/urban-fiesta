2383a98a943da47f6f0dd3a78345d55468c9354b
Caio Raposo <caioraposo@protonmail.com>
2020-05-05 18:19:09 -0300

    Add password_hash attribute to user

--- src/no12/models.py
+++ src/no12/models.py
@@ -1,15 +1,17 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 from .extensions import db
 
 
-class User(db.Document):
-    email = db.StringField(required=True)
+class User(UserMixin, db.Document):
+    email = db.EmailField(required=True, unique=True)
     first_name = db.StringField(max_length=50)
     last_name = db.StringField(max_length=50)
+    password_hash = db.StringField(max_length=128)
 
     def set_password(self, password: str) -> None:
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password: str) -> None:
+    def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
